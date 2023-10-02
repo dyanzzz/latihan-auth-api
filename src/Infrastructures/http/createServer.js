990982d788acf08b1332b985beea129c3e0522b5
Dian Tri Budianto <dian.budianto@fastratabuana.co.id>
2023-10-02 16:35:34 +0700

    update testing ci

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,69 +1,69 @@
-const Hapi = require('@hapi/hapi');
-const users = require('../../Interfaces/http/api/users');
-const authentications = require('../../Interfaces/http/api/authentications');
-const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
-const ClientError = require('../../Commons/exceptions/ClientError');
+const Hapi = require('@hapi/hapi')
+const users = require('../../Interfaces/http/api/users')
+const authentications = require('../../Interfaces/http/api/authentications')
+const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator')
+const ClientError = require('../../Commons/exceptions/ClientError')
 
 const createServer = async (container) => {
-  const server = Hapi.server({
-    host: process.env.HOST,
-    port: process.env.PORT,
-  });
+	const server = Hapi.server({
+		host: process.env.HOST,
+		port: process.env.PORT,
+	})
 
-  await server.register([
-    {
-      plugin: users,
-      options: { container },
-    },
-    {
-      plugin: authentications,
-      options: { container },
-    },
-  ]);
+	await server.register([
+		{
+			plugin: users,
+			options: { container },
+		},
+		{
+			plugin: authentications,
+			options: { container },
+		},
+	])
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: () => ({
-     value: 'Hello World MasBro! Siaaap',
-    }),
-  });
+	server.route({
+		method: 'GET',
+		path: '/',
+		handler: () => ({
+			value: 'Hello World MasBro! testing ci',
+		}),
+	})
 
-  server.ext('onPreResponse', (request, h) => {
-    // mendapatkan konteks response dari request
-    const { response } = request;
-    if (response instanceof Error) {
-      // bila response tersebut error, tangani sesuai kebutuhan
-      const translatedError = DomainErrorTranslator.translate(response);
+	server.ext('onPreResponse', (request, h) => {
+		// mendapatkan konteks response dari request
+		const { response } = request
+		if (response instanceof Error) {
+			// bila response tersebut error, tangani sesuai kebutuhan
+			const translatedError = DomainErrorTranslator.translate(response)
 
-      // penanganan client error secara internal.
-      if (translatedError instanceof ClientError) {
-        const newResponse = h.response({
-          status: 'fail',
-          message: translatedError.message,
-        });
-        newResponse.code(translatedError.statusCode);
-        return newResponse;
-      }
+			// penanganan client error secara internal.
+			if (translatedError instanceof ClientError) {
+				const newResponse = h.response({
+					status: 'fail',
+					message: translatedError.message,
+				})
+				newResponse.code(translatedError.statusCode)
+				return newResponse
+			}
 
-      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
-      if (!translatedError.isServer) {
-        return h.continue;
-      }
+			// mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+			if (!translatedError.isServer) {
+				return h.continue
+			}
 
-      // penanganan server error sesuai kebutuhan
-      const newResponse = h.response({
-        status: 'error',
-        message: 'terjadi kegagalan pada server kami',
-      });
-      newResponse.code(500);
-      return newResponse;
-    }
-    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
-    return h.continue;
-  });
+			// penanganan server error sesuai kebutuhan
+			const newResponse = h.response({
+				status: 'error',
+				message: 'terjadi kegagalan pada server kami',
+			})
+			newResponse.code(500)
+			return newResponse
+		}
+		// jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+		return h.continue
+	})
 
-  return server;
+	return server
 }
 
-module.exports = createServer;
+module.exports = createServer
